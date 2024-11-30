cd6069c81ffe894a9d49edd0dee3c89aa6a03f61
mtuyar <mtuyar@gmail.com>
2024-11-30 14:45:45 +0300

    docker updated

--- dbConfig.js
+++ dbConfig.js
@@ -1,13 +1,14 @@
-const config = {
-  user: 'sa',
-  password: 'YourStrong@Passw0rd',
-  server: 'localhost',
-  database: 'master',
-  options: {
-    encrypt: true,
-    trustServerCertificate: true
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false
   }
-};
+});
 
-
-module.exports = config;
+module.exports = pool;
